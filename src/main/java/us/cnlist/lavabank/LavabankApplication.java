7665e6840f79a5cea65f4064320e112774341a84
Sergei W. Adonev <admin@cnlist.us>
2019-11-06 00:45:09 +0500

    Added servletContext

--- src/main/java/us/cnlist/lavabank/LavabankApplication.java
+++ src/main/java/us/cnlist/lavabank/LavabankApplication.java
@@ -2,10 +2,18 @@ package us.cnlist.lavabank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class LavabankApplication {
+public class LavabankApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LavabankApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(LavabankApplication.class, args);
